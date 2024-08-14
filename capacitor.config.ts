6a3ec2b7e304c5db7bf6bed00bdef23844d19392
Eric Horodyski <horodyski@ionic.io>
2024-08-14 13:08:38 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iab-test',
+  webDir: 'dist'
+};
+
+export default config;
