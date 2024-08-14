5a8e2068a6372c51e1db28775df678a17178bb02
Eric Horodyski <horodyski@ionic.io>
2024-08-14 15:18:07 -0400

    Initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import {
+  InAppBrowser,
+  iOSAnimation,
+  iOSViewStyle,
+  ToolbarPosition,
+} from "@capacitor/inappbrowser";
+import "./Home.css";
+
+const launchInAppBrowser = async () => {
+  await InAppBrowser.openInWebView({
+    url: "https://google.com",
+    options: {
+      showURL: false,
+      showToolbar: false,
+      clearCache: false,
+      clearSessionCache: false,
+      mediaPlaybackRequiresUserAction: false,
+      closeButtonText: "Done",
+      toolbarPosition: ToolbarPosition.TOP,
+      showNavigationButtons: false,
+      leftToRight: true,
+      android: {
+        allowZoom: false,
+        hardwareBack: true,
+        pauseMedia: true,
+      },
+      iOS: {
+        allowOverScroll: false,
+        enableViewportScale: false,
+        allowInLineMediaPlayback: false,
+        surpressIncrementalRendering: true,
+        viewStyle: iOSViewStyle.FULL_SCREEN,
+        animationEffect: iOSAnimation.CROSS_DISSOLVE,
+      },
+    },
+  });
+};
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +58,13 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton
+          style={{ marginTop: "150px" }}
+          expand="block"
+          onClick={launchInAppBrowser}
+        >
+          Link to terms and conditions
+        </IonButton>
       </IonContent>
     </IonPage>
   );
